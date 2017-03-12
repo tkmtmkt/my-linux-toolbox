2bba9114fe134bdd5ea54d3824114435b20c5c00
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-03-12 23:44:03 +0900

    add: tar追加

---
+++ common/opt/toolbox/recipe/build-tar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+source $SCRIPT_DIR/build-common.sh
+
+pushd $BUILD_DIR
+
+TARGET=tar-1.29
+ARCHIVE=$SRC_DIR/$TARGET.tar.gz
+DOWNLOAD_URL=http://ftp.gnu.org/gnu/tar/$TARGET.tar.gz
+[[ ! -e $ARCHIVE ]] && wget -O $ARCHIVE $DOWNLOAD_URL
+[[ ! -e $TARGET ]] && tar zxf $ARCHIVE
+pushd $TARGET
+./configure --prefix=$PREFIX \
+            FORCE_UNSAFE_CONFIGURE=1
+make && make install
+popd
+
+popd
--- common/opt/toolbox/recipe/build.sh
+++ common/opt/toolbox/recipe/build.sh
@@ -4,4 +4,5 @@ SCRIPT_DIR=$(cd $(dirname $0);pwd)
 $SCRIPT_DIR/build-m4.sh
 $SCRIPT_DIR/build-autoconf.sh
 $SCRIPT_DIR/build-ncurses.sh
+$SCRIPT_DIR/build-tar.sh
 $SCRIPT_DIR/build-vim.sh
