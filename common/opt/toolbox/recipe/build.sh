2bae6f260fd687602e32f6f1280fce6ae4987ac6
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-03-19 14:24:08 +0900

    clean: add comment

--- common/opt/toolbox/recipe/build.sh
+++ common/opt/toolbox/recipe/build.sh
@@ -7,15 +7,18 @@ $SCRIPT_DIR/build-m4.sh
 $SCRIPT_DIR/build-autoconf.sh
 $SCRIPT_DIR/build-automake.sh
 
+# archive library
 $SCRIPT_DIR/build-zlib.sh
 $SCRIPT_DIR/build-bzip.sh
 $SCRIPT_DIR/build-xz.sh
 
 $SCRIPT_DIR/build-tar.sh
 
+# vim
 $SCRIPT_DIR/build-ncurses.sh
 $SCRIPT_DIR/build-vim.sh
 
+# git
 $SCRIPT_DIR/build-gettext.sh
 $SCRIPT_DIR/build-expat.sh
 $SCRIPT_DIR/build-openssl.sh
@@ -30,9 +33,11 @@ $SCRIPT_DIR/build-libksba.sh
 $SCRIPT_DIR/build-pth.sh
 $SCRIPT_DIR/build-gnupg.sh
 
+# tmux
 #$SCRIPT_DIR/build-libevent.sh
 #$SCRIPT_DIR/build-tmux.sh
 
+# other utils
 $SCRIPT_DIR/build-highway.sh
 $SCRIPT_DIR/build-global.sh
 $SCRIPT_DIR/build-p7zip.sh
