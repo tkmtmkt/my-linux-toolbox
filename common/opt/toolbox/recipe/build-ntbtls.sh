3ba8182d3ec3ef6c4b52ca0425dc05d26c75115b
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 18:02:59 +0900

    add: ntbtls

---
+++ common/opt/toolbox/recipe/build-ntbtls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+source $SCRIPT_DIR/build-common.sh
+
+pushd $BUILD_DIR
+
+TARGET=ntbtls-0.1.2
+ARCHIVE=$ARCH_DIR/$TARGET.tar.bz2
+DOWNLOAD_URL=https://gnupg.org/ftp/gcrypt/ntbtls/$TARGET.tar.bz2
+[[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
+[[ ! -e $TARGET ]] && tar jxf $ARCHIVE
+pushd $TARGET
+./configure --prefix=$PREFIX \
+            --enable-static \
+            --disable-shared \
+            --with-libgpg-error-prefix=$PREFIX \
+            --with-libgcrypt-prefix=$PREFIX \
+            --with-ksba-prefix=$PREFIX \
+            --with-zlib=$PREFIX
+make && make install
+popd
+
+popd
