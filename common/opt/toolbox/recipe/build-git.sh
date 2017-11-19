a21045a4e5e57d39642265dd865999d9e49d6e9d
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 19:58:07 +0900

    update: git (2.15.0 <- 2.12.0)

--- common/opt/toolbox/recipe/build-git.sh
+++ common/opt/toolbox/recipe/build-git.sh
@@ -4,14 +4,15 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=git-2.12.0
+TARGET=git-2.15.0
 ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
-DOWNLOAD_URL=https://github.com/git/git/archive/v2.12.0.tar.gz
+DOWNLOAD_URL=https://github.com/git/git/archive/v2.15.0.tar.gz
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar zxf $ARCHIVE
 pushd $TARGET
 make configure
 ./configure --prefix=$PREFIX \
+            --with-zlib=$PREFIX/lib \
             --with-perl=$PREFIX/bin/perl
 make all
 make install
