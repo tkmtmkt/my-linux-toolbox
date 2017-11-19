a4a69c30208b6f4cce7188af5e258bd310d44ba5
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 13:36:59 +0900

    update: openssl (1.1.0g <- 1.1.0e)

--- common/opt/toolbox/recipe/build-openssl.sh
+++ common/opt/toolbox/recipe/build-openssl.sh
@@ -4,13 +4,15 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=openssl-1.1.0e
+TARGET=openssl-1.1.0g
 ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
 DOWNLOAD_URL=https://www.openssl.org/source/$TARGET.tar.gz
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar zxf $ARCHIVE
 pushd $TARGET
-./config --prefix=$PREFIX shared zlib \
+./config no-shared zlib \
+         --prefix=$PREFIX \
+         --openssldir=$PREFIX \
          --with-zlib-include=$PREFIX/include \
          --with-zlib-lib=$PREFIX/lib
 make && make install
