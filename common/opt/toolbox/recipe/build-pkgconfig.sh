53266758309b30a388e51df1c956ea890ec6793b
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 11:14:27 +0900

    update: pkgtool (0.29.2 <- 0.29.1)

--- common/opt/toolbox/recipe/build-pkgconfig.sh
+++ common/opt/toolbox/recipe/build-pkgconfig.sh
@@ -4,14 +4,16 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=pkg-config-0.29.1
+TARGET=pkg-config-0.29.2
 ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
 DOWNLOAD_URL=http://pkg-config.freedesktop.org/releases/$TARGET.tar.gz
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar zxf $ARCHIVE
 pushd $TARGET
 ./configure --prefix=$PREFIX \
-            --with-internal-glib
+            --with-internal-glib \
+            --disable-compile-warnings \
+            --disable-host-tool
 make && make install
 popd
 
