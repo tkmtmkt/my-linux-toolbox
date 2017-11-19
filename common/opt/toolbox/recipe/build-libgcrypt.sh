5933746cc67616361055dcbbe0e68a2ff300ae2f
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 18:01:07 +0900

    update: libgcrypt (1.8.1 <- 1.7.6)

--- common/opt/toolbox/recipe/build-libgcrypt.sh
+++ common/opt/toolbox/recipe/build-libgcrypt.sh
@@ -4,13 +4,17 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=libgcrypt-1.7.6
+TARGET=libgcrypt-1.8.1
 ARCHIVE=$ARCH_DIR/$TARGET.tar.bz2
 DOWNLOAD_URL=https://www.gnupg.org/ftp/gcrypt/libgcrypt/$TARGET.tar.bz2
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar jxf $ARCHIVE
 pushd $TARGET
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+            --enable-static \
+            --disable-shared \
+            --with-libgpg-error-prefix=$PREFIX \
+            --with-pth-prefix=$PREFIX
 make && make install
 popd
 
