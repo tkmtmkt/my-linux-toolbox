1dd4515815c312e923160756f2075341e3634145
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 16:48:13 +0900

    update: expat (2.2.5 <- 2.2.0)

--- common/opt/toolbox/recipe/build-expat.sh
+++ common/opt/toolbox/recipe/build-expat.sh
@@ -4,13 +4,15 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=expat-2.2.0
+TARGET=expat-2.2.5
 ARCHIVE=$ARCH_DIR/$TARGET.tar.bz2
-DOWNLOAD_URL=https://sourceforge.net/projects/expat/files/expat/2.2.0/$TARGET.tar.bz2
+DOWNLOAD_URL=https://sourceforge.net/projects/expat/files/expat/2.2.5/$TARGET.tar.bz2
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar jxf $ARCHIVE
 pushd $TARGET
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+            --enable-static \
+            --disable-shared
 make && make install
 popd
 
