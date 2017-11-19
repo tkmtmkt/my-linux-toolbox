e991028386c46c687cc48047bad53233d82c6c45
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 18:02:22 +0900

    update: libassuan (2.4.4 <- 2.4.3)

--- common/opt/toolbox/recipe/build-libassuan.sh
+++ common/opt/toolbox/recipe/build-libassuan.sh
@@ -4,13 +4,16 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=libassuan-2.4.3
+TARGET=libassuan-2.4.4
 ARCHIVE=$ARCH_DIR/$TARGET.tar.bz2
 DOWNLOAD_URL=https://www.gnupg.org/ftp/gcrypt/libassuan/$TARGET.tar.bz2
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar jxf $ARCHIVE
 pushd $TARGET
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX \
+            --enable-static \
+            --disable-shared \
+            --with-libgpg-error-prefix=$PREFIX
 make && make install
 popd
 
