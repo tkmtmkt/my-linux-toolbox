4a98c0b85dae8d453e1b603b04297f70e221c1ce
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 11:59:14 +0900

    update: global (6.5.7 <- 6.5.6)

--- common/opt/toolbox/recipe/build-global.sh
+++ common/opt/toolbox/recipe/build-global.sh
@@ -4,7 +4,7 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=global-6.5.6
+TARGET=global-6.5.7
 ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
 DOWNLOAD_URL=http://ftp.gnu.org/gnu/global/$TARGET.tar.gz
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
