c9e15c8abffd3dd230e1e6ba18b44b80bb9b87fc
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 15:37:47 +0900

    add: sqlite3

---
+++ common/opt/toolbox/recipe/build-sqlite3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+source $SCRIPT_DIR/build-common.sh
+
+pushd $BUILD_DIR
+
+TARGET=sqlite-autoconf-3210000
+ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
+DOWNLOAD_URL=https://www.sqlite.org/2017/$TARGET.tar.gz
+[[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
+[[ ! -e $TARGET ]] && tar zxf $ARCHIVE
+pushd $TARGET
+./configure --prefix=$PREFIX \
+            --disable-shared
+make && make install
+popd
+
+popd
