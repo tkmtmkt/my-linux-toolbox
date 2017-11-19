54fd887bbfcb94819ebb79edf91569a1b1574bf0
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-19 15:54:42 +0900

    update: vim (v8.0.1307 <- v8.0.0433)

--- common/opt/toolbox/recipe/build-vim.sh
+++ common/opt/toolbox/recipe/build-vim.sh
@@ -4,15 +4,17 @@ source $SCRIPT_DIR/build-common.sh
 
 pushd $BUILD_DIR
 
-TARGET=vim-8.0.0433
+TARGET=vim-8.0.1307
 ARCHIVE=$ARCH_DIR/$TARGET.tar.gz
-DOWNLOAD_URL=https://github.com/vim/vim/archive/v8.0.0433.tar.gz
+DOWNLOAD_URL=https://github.com/vim/vim/archive/v8.0.1307.tar.gz
 [[ ! -e $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar zxf $ARCHIVE
 pushd $TARGET
 ./configure --prefix=$PREFIX \
             --disable-selinux \
-            --enable-python3interp
+            --enable-multibyte \
+            --enable-fail-if-missing \
+            --with-features=huge
 make && make install
 ln -sf vim $PREFIX/bin/vi
 popd
