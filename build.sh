5f56033c4ea919ce7a47668ee4890017cab6c877
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-23 21:04:56 +0900

    fix: ビルド失敗したら途中で中断する
    * ビルド失敗した場合、失敗がすぐわかるように中断するようにした

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#docker-compose run --rm centos5 /opt/toolbox/recipe/build.sh 2>&1 | tee build-centos5.log
-docker-compose run --rm centos6 /opt/toolbox/recipe/build.sh 2>&1 | tee build-centos6.log
-#docker-compose run --rm centos7 /opt/toolbox/recipe/build.sh 2>&1 | tee build-centos7.log
+#docker-compose run --rm centos5 /opt/toolbox/recipe/build.sh > build-centos5.log 2>&1
+docker-compose run --rm centos6 /opt/toolbox/recipe/build.sh > build-centos6.log 2>&1
+#docker-compose run --rm centos7 /opt/toolbox/recipe/build.sh > build-centos7.log 2>&1
