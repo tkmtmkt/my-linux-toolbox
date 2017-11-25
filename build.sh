acd7ccc2101d571d7369b0629bc5f256a16a7fe5
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-11-25 22:46:39 +0900

    add: Ubuntu 16.04ビルド環境追加

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #docker-compose run --rm centos5 /opt/toolbox/recipe/build.sh > build-centos5.log 2>&1
-docker-compose run --rm centos6 /opt/toolbox/recipe/build.sh > build-centos6.log 2>&1
+#docker-compose run --rm centos6 /opt/toolbox/recipe/build.sh > build-centos6.log 2>&1
 #docker-compose run --rm centos7 /opt/toolbox/recipe/build.sh > build-centos7.log 2>&1
+docker-compose run --rm ubuntu1604 /opt/toolbox/recipe/build.sh > build-ubuntu1604.log 2>&1
