41538a4fc99190826af22014964924fb983d4044
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-12-10 21:15:23 +0900

    fix: 引数判定処理修正

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-if [[ -z "$1" || ! -d "$SCRIPT_DIR/$1" ]]; then
-  echo "usage: $0 {container_name}"
-  exit 1
-fi
-
-docker-compose run --rm $1 /usr/local/toolbox/scripts/build-all.sh
+pushd $SCRIPT_DIR
+case "$1" in
+  centos[5-7]|ubuntu1604)
+    docker-compose run --rm $1 /usr/local/toolbox/scripts/build-all.sh
+    ;;
+  *)
+    echo "Usage: $0 [centos5|centos6|centos7|ubuntu1604]"
+    ;;
+esac
+popd
