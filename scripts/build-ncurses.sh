30e3e8053a4a7e3e8417fcfdfbc334aeb38c0135
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-12-14 23:54:10 +0900

    fix: ncurseswのシンボリックリンク不足分追加

--- scripts/build-ncurses.sh
+++ scripts/build-ncurses.sh
@@ -12,19 +12,23 @@ DOWNLOAD_URL=http://ftp.gnu.org/gnu/ncurses/$TARGET.tar.gz
 
 pushd $TARGET
 ./configure --prefix=$PREFIX \
-            --without-manpages \
             --with-pkg-config=$PREFIX/bin/pkg-config \
             --with-pkg-config-libdir=$PREFIX/lib/pkgconfig \
-            --enable-pc-files \
             --with-shared \
             --without-debug \
+            --without-manpages \
+            --enable-pc-files \
             --enable-rpath \
             --enable-widec \
             --enable-ext-colors
 make && make install &&
-ln -sf $PREFIX/lib/libncurses{w,}.a &&
-ln -sf $PREFIX/lib/libncurses{w,}.so &&
-ln -sf $PREFIX/lib/libncurses{w,}.so.6
+for lib in ncurses form panel menu
+do
+  ln -sf lib${lib}w.a    $PREFIX/lib/lib${lib}.a
+  ln -sf lib${lib}w.so   $PREFIX/lib/lib${lib}.so
+  ln -sf lib${lib}w.so.6 $PREFIX/lib/lib${lib}.so.6
+  ln -sf lib${lib}w.pc   $PREFIX/lib/pkgconfig/${lib}.pc
+done
 RESULT=$?
 popd
 
