7d9b83227fe57b25a333c7978c65fcc4dc102f72
Takamatsu Makoto <tkmtmkt@gmail.com>
2017-12-14 13:47:48 +0900

    modify: curlビルドにCA証明書のダウロードを追加

--- scripts/build-curl.sh
+++ scripts/build-curl.sh
@@ -9,13 +9,19 @@ DOWNLOAD_URL=https://curl.haxx.se/download/$TARGET.tar.gz
 [[ ! -s $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 [[ ! -e $TARGET ]] && tar zxf $ARCHIVE
 
+CA_CERT=$ARCH_DIR/cacert.pem
+[[ ! -s $CA_CERT ]] && wget -O $CA_CERT http://curl.haxx.se/ca/cacert.pem
+CA_PATH=$PREFIX/ssl/certs
+CA_BUNDLE=$CA_PATH/ca-bundle.crt
+cp $CA_CERT $CA_BUNDLE
+
 pushd $TARGET
 ./configure --prefix=$PREFIX \
             --disable-manual \
             --enable-libcurl-option \
             --with-ssl=$PREFIX/ssl \
-            --with-ca-path=$PREFIX/ssl \
-            --with-ca-bundle=$PREFIX/ssl/ca-bundle.crt
+            --with-ca-path=$CA_PATH \
+            --with-ca-bundle=$CA_BUNDLE
 make && make install
 RESULT=$?
 popd
