799844e17acb9bcc471241d91144f0c32985e9e1
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-28 02:25:14 +0900

    clean: 処理順序変更

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 
-pushd $SCRIPT_DIR
 case "$1" in
   centos[5-7]|ubuntu1604)
+    pushd $SCRIPT_DIR
     docker-compose run --rm $1 /usr/local/toolbox/scripts/build-all.sh
+    popd
     ;;
   *)
     echo "Usage: $0 [centos5|centos6|centos7|ubuntu1604]"
     ;;
 esac
-popd
