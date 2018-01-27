0e8dae8b043f02daaf51e98a6b47c95828dfc21e
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-28 01:22:39 +0900

    clean: ダウンロードとビルドの処理記述位置を整理

--- scripts/build-readline.sh
+++ scripts/build-readline.sh
@@ -9,23 +9,22 @@ ARCHIVE=$ARCHIVES_DIR/$TARGET.tar.gz
 DOWNLOAD_URL=http://ftp.gnu.org/gnu/readline/$TARGET.tar.gz
 [[ ! -s $ARCHIVE ]] && wget --no-check-certificate -O $ARCHIVE $DOWNLOAD_URL
 
+for n in 001 002 003
+do
+  PATCH="readline70-$n"
+  [[ ! -s "$ARCHIVES_DIR/$PATCH" ]] && wget http://ftp.gnu.org/gnu/readline/readline-7.0-patches/$PATCH -O "$ARCHIVES_DIR/$PATCH"
+done
+
 # build
 pushd $BUILD_DIR
 [[ -d $TARGET ]] && rm -rf $TARGET
 tar zxf $ARCHIVE
 
-# patches
+pushd $TARGET
 for n in 001 002 003
 do
-  PATCH="readline70-$n"
-  [[ ! -s "$ARCHIVES_DIR/$PATCH" ]] && wget http://ftp.gnu.org/gnu/readline/readline-7.0-patches/$PATCH -O "$ARCHIVES_DIR/$PATCH"
-  [[ ! -s "$TARGET/$PATCH" ]] && cp "$ARCHIVES_DIR/$PATCH" "$TARGET/$PATCH"
-done
-
-pushd $TARGET
-patch -p0 < readline70-001 &&
-patch -p0 < readline70-002 &&
-patch -p0 < readline70-003 &&
+  patch -p0 < $ARCHIVES_DIR/readline70-$n
+done &&
 ./configure --prefix=$PREFIX \
             --enable-multibyte &&
 make && make install
