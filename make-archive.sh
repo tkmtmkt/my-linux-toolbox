662b268b66b45882e2343b3c1c7fad7ce80d99b2
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-29 01:17:01 +0900

    fix: アーカイブファイル名に日付付与

--- make-archive.sh
+++ make-archive.sh
@@ -2,10 +2,9 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 BASE_NAME=$(basename $SCRIPT_DIR)
-ARCHIVE=$BASE_NAME.tar.gz
 
-tar czf $ARCHIVE -C $BASE_DIR \
-    --exclude=$BASE_NAME/$ARCHIVE \
+tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $BASE_DIR \
+    --exclude=$BASE_NAME/$BASE_NAME-*.tar.gz \
     --exclude=$BASE_NAME/build \
     --exclude=$BASE_NAME/docker/*/{home,usr} \
     --exclude=$BASE_NAME/log/*.log \
