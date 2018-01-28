1cc8cfa0896981e3bded9a17c33e47517f3c2138
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-01-28 23:11:20 +0900

    fix: build-all.sh実行時の処理状況出力追加

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,59 +5,74 @@ LOG_DIR=$BASE_DIR/log
 
 [[ ! -d "$LOG_DIR" ]] && mkdir $LOG_DIR
 
-# base
-$SCRIPT_DIR/build-pkgconfig.sh    > $LOG_DIR/result-pkgconfig-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-perl.sh         > $LOG_DIR/result-perl-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-m4.sh           > $LOG_DIR/result-m4-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-autoconf.sh     > $LOG_DIR/result-autoconf-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-automake.sh     > $LOG_DIR/result-automake-$(date +%Y%m%d%H%M).log 2>&1 &&
+while read PKG
+do
+  [[ $(echo $PKG | grep -e "^#") ]] && continue
+  [[ -z $PKG ]] && continue
+  echo -e "\n### build-$PKG"
+  time $SCRIPT_DIR/build-$PKG.sh > $LOG_DIR/result-$PKG-$(date +%Y%m%d%H%M).log 2>&1
+  RESULT=$?
+  if [[ $RESULT -eq 0 ]]; then
+    echo "Success"
+  else
+    echo "Failure($RESULT)"
+    exit $RESULT
+  fi
+done <<'EOS'
+### base
+pkgconfig
+perl
+m4
+autoconf
+automake
 
-# library
-$SCRIPT_DIR/build-zlib.sh         > $LOG_DIR/result-zlib-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-bzip.sh         > $LOG_DIR/result-bzip-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-xz.sh           > $LOG_DIR/result-xz-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-ncurses.sh      > $LOG_DIR/result-ncurses-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-gettext.sh      > $LOG_DIR/result-gettext-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-readline.sh     > $LOG_DIR/result-readline-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-expat.sh        > $LOG_DIR/result-expat-$(date +%Y%m%d%H%M).log 2>&1 &&
+### library
+zlib
+bzip
+xz
+ncurses
+gettext
+readline
+expat
 
-# util
-$SCRIPT_DIR/build-openssl.sh      > $LOG_DIR/result-openssl-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-curl.sh         > $LOG_DIR/result-curl-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-tar.sh          > $LOG_DIR/result-tar-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-p7zip.sh        > $LOG_DIR/result-p7zip-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-highway.sh      > $LOG_DIR/result-highway-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-global.sh       > $LOG_DIR/result-global-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-sqlite3.sh      > $LOG_DIR/result-sqlite3-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-nkf.sh          > $LOG_DIR/result-nkf-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-jq.sh           > $LOG_DIR/result-jq-$(date +%Y%m%d%H%M).log 2>&1 &&
+### util
+openssl
+curl
+tar
+p7zip
+highway
+global
+sqlite3
+nkf
+jq
 
-# gcc
-$SCRIPT_DIR/build-gmp.sh          > $LOG_DIR/result-gmp-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-mpfr.sh         > $LOG_DIR/result-mpfr-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-mpc.sh          > $LOG_DIR/result-mpc-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-gcc.sh          > $LOG_DIR/result-gcc-$(date +%Y%m%d%H%M).log 2>&1 &&
+### gcc
+gmp
+mpfr
+mpc
+gcc
 
-# language
-$SCRIPT_DIR/build-python.sh       > $LOG_DIR/result-python-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-ruby.sh         > $LOG_DIR/result-ruby-$(date +%Y%m%d%H%M).log 2>&1 &&
+### language
+python
+ruby
 
-# vim
-$SCRIPT_DIR/build-vim.sh          > $LOG_DIR/result-vim-$(date +%Y%m%d%H%M).log 2>&1 &&
+### vim
+vim
 
-# git
-$SCRIPT_DIR/build-git.sh          > $LOG_DIR/result-git-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-tig.sh          > $LOG_DIR/result-tig-$(date +%Y%m%d%H%M).log 2>&1 &&
+### git
+git
+tig
 
-# tmux
-$SCRIPT_DIR/build-libevent.sh     > $LOG_DIR/result-libevent-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-tmux.sh         > $LOG_DIR/result-tmux-$(date +%Y%m%d%H%M).log 2>&1 &&
+### tmux
+libevent
+tmux
 
-# GnuPG
-$SCRIPT_DIR/build-libgpg-error.sh > $LOG_DIR/result-libgpg-error-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-npth.sh         > $LOG_DIR/result-npth-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-libgcrypt.sh    > $LOG_DIR/result-libgcrypt-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-libksba.sh      > $LOG_DIR/result-libksba-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-libassuan.sh    > $LOG_DIR/result-libassuan-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-ntbtls.sh       > $LOG_DIR/result-ntbtls-$(date +%Y%m%d%H%M).log 2>&1 &&
-$SCRIPT_DIR/build-gnupg.sh        > $LOG_DIR/result-gnupg-$(date +%Y%m%d%H%M).log 2>&1
+### GnuPG
+libgpg-error
+npth
+libgcrypt
+libksba
+libassuan
+ntbtls
+gnupg
+EOS
