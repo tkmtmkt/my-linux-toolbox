d06a8d649efd583a8b162cb56d2b475de75c5d05
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-03-17 09:31:46 +0900

    add: dockerでビルドしたtoolboxをアーカイブするスクリプト

---
+++ docker/make-archive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+case "$1" in
+  centos[67]|ubuntu1604)
+    BASE_NAME=toolbox-$1
+    PARENT_DIR=$SCRIPT_DIR/$1/opt
+
+    tar czf $BASE_NAME-$(date +%Y%m%d).tar.gz -C $PARENT_DIR \
+        --exclude=toolbox/$BASE_NAME-*.tar.gz \
+        --exclude=toolbox/workspace \
+        toolbox
+    ;;
+  *)
+    echo "Usage: $0 [centos6|centos7|ubuntu1604]"
+    exit 1
+    ;;
+esac
