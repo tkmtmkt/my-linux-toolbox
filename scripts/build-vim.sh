4f1897874d2c7658d3fdb820bfa3661d60fb8b48
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-03-17 10:10:45 +0900

    fix: vimの日本語ヘルプ追加

--- scripts/build-vim.sh
+++ scripts/build-vim.sh
@@ -9,12 +9,20 @@ ARCHIVE=$ARCHIVES_DIR/$TARGET.tar.gz
 DOWNLOAD_URL=https://github.com/vim/vim/archive/v8.0.1307.tar.gz
 [[ ! -s $ARCHIVE ]] && curl -ksSL $DOWNLOAD_URL -o $ARCHIVE
 
+VIMDOC_JA=vimdoc-ja.tar.gz
+[[ ! -s "$ARCHIVES_DIR/$VIMDOC_JA" ]] &&
+  curl -ksSL https://github.com/vim-jp/vimdoc-ja/archive/master.tar.gz -o "$ARCHIVES_DIR/$VIMDOC_JA"
+
 # build
 pushd $BUILD_DIR
 [[ -d $TARGET ]] && rm -rf $TARGET
 tar zxf $ARCHIVE
 
 pushd $TARGET
+tar xzf "$ARCHIVES_DIR/$VIMDOC_JA" &&
+cp -p vimdoc-ja-master/doc/* runtime/doc/ &&
+cp -p vimdoc-ja-master/syntax/* runtime/syntax/ &&
+
 ./configure --prefix=$PREFIX \
             --enable-fail-if-missing \
             --enable-python3interp=yes \
