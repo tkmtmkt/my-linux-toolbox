0aa7b88276c7ae817c053aa4b91fdf3a0f883cda
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-05-20 02:28:46 +0900

    fix: toolboxビルド用のツールをbinに含めないように修正

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -4,14 +4,15 @@ BASE_DIR=$(cd $SCRIPT_DIR/..;pwd)
 
 export LANG=C
 export PREFIX=$(cd $BASE_DIR/..;pwd)
-export PKG_CONFIG=$PREFIX/bin/pkg-config
+export PREFIX_BUILDTOOL=$PREFIX/buildtool
+export PATH=$PREFIX_BUILDTOOL/bin:$PREFIX/bin:$PATH
+export PKG_CONFIG=$PREFIX_BUILDTOOL/bin/pkg-config
 export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig
 export CFLAGS="-I$PREFIX/include"
 export CPPFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export LT_SYS_LIBRARY_PATH=$PREFIX/lib
 export LD_LIBRARY_PATH=$PREFIX/lib
-export PATH=$PREFIX/bin:$PATH
 export MANPATH=$PREFIX/man:
 
 export ARCHIVES_DIR=$BASE_DIR/archives
