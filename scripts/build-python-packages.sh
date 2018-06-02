422648f9add64651a2801ddedc320e3aed3f84ad
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-06-02 21:59:00 +0900

    fix: python packages コメント追記

--- scripts/build-python-packages.sh
+++ scripts/build-python-packages.sh
@@ -5,14 +5,17 @@ source $SCRIPT_DIR/build-common.sh
 # python packages
 if [[ -d "$ARCHIVES_DIR/wheels" ]]
 then
-  pip install --no-deps --no-index $(find $ARCHIVES_DIR/wheels -name pip-*)
-  #
+  # offline install (pip & wheel)
+  pip install --no-deps --no-index $(find $ARCHIVES_DIR/wheels -name pip-* -o -name wheel-*)
+  # offline install (other)
   pip install --no-deps --no-index -r $SCRIPT_DIR/requirements.txt -f $ARCHIVES_DIR/wheels
 else
+  # online install (pip & wheel)
   pip install --upgrade pip wheel
   pip wheel pip wheel -w $ARCHIVES_DIR/wheels
-  #
+  # online install (other)
   pip install -r $SCRIPT_DIR/requirements.in.txt
+  # download packages -> wheels directory
   pip freeze > $SCRIPT_DIR/requirements.txt
   pip wheel -r $SCRIPT_DIR/requirements.txt -w $ARCHIVES_DIR/wheels
 fi
