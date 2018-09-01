30982e8180c13ec2eaee4415fabee4118a463839
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-09-01 15:24:51 +0900

    fix: trivial

--- scripts/build-python-packages.sh
+++ scripts/build-python-packages.sh
@@ -13,13 +13,13 @@ then
 else
   # online install (pip & wheel)
   pip3 install --upgrade pip wheel
-  pip3 wheel pip wheel -w $ARCHIVES_DIR/wheels
 
   # online install (other)
   pip3 install -r $SCRIPT_DIR/requirements.in.txt
+  pip3 freeze > $SCRIPT_DIR/requirements.txt
 
   # download packages -> wheels directory
-  pip3 freeze > $SCRIPT_DIR/requirements.txt
+  pip3 wheel pip wheel -w $ARCHIVES_DIR/wheels
   pip3 wheel -r $SCRIPT_DIR/requirements.txt -w $ARCHIVES_DIR/wheels
 
   # TODO: Provisional action until supervisor version 4 is released.
