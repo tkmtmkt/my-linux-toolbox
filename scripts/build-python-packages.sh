d39d7b645ac0c3baddd3b8240a165febfb0e7fe2
Takamatsu Makoto <tkmtmkt@gmail.com>
2018-10-21 00:55:17 +0900

    fix: pythonパッケージインストールが途中失敗した場合に処理を中断する

--- scripts/build-python-packages.sh
+++ scripts/build-python-packages.sh
@@ -2,34 +2,47 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 source $SCRIPT_DIR/build-common.sh
 
+WHEELS_DIR=$ARCHIVES_DIR/wheels
+
+pushd $BUILD_DIR
+
 # python packages
 if [[ "$OFFLINE" = "yes" ]]
 then
   # offline install (pip & wheel)
-  pip3 install --no-deps --no-index $(find $ARCHIVES_DIR/wheels -name pip-* -o -name wheel-*)
+  pip3 install --no-deps --no-index $(find $WHEELS_DIR -name pip-* -o -name wheel-*) &&
 
   # offline install (other)
-  pip3 install --no-deps --no-index -f $ARCHIVES_DIR/wheels -r $SCRIPT_DIR/requirements.txt
+  pip3 install --no-deps --no-index -f $WHEELS_DIR -r $SCRIPT_DIR/requirements.txt
 else
   # online install (pip & wheel)
-  pip3 install --upgrade pip wheel
+  pip3 install --upgrade pip wheel &&
 
   # online install (other)
-  pip3 install -r $SCRIPT_DIR/requirements.in.txt
-  pip3 install Cartopy Shapely
-  pip3 freeze > $SCRIPT_DIR/requirements.txt
+  pip3 install -r $SCRIPT_DIR/requirements.in.txt &&
+  pip3 install Cartopy Shapely &&
+  pip3 freeze > $SCRIPT_DIR/requirements.txt &&
 
   # download packages -> wheels directory
-  pip3 wheel -w $ARCHIVES_DIR/wheels pip wheel
-  pip3 wheel -w $ARCHIVES_DIR/wheels -r $SCRIPT_DIR/requirements.txt
+  if [[ -d $WHEELS_DIR ]]; then rm -rf $WHEELS_DIR; fi &&
+  pip3 wheel -w $WHEELS_DIR pip wheel &&
+  pip3 wheel -w $WHEELS_DIR -r $SCRIPT_DIR/requirements.txt &&
 
   # TODO: Provisional action until supervisor version 4 is released.
-  pip3 install git+https://github.com/Supervisor/supervisor
-  pip3 wheel -w $ARCHIVES_DIR/wheels git+https://github.com/Supervisor/supervisor
-  pip3 freeze > $SCRIPT_DIR/requirements.txt
+  pip3 install git+https://github.com/Supervisor/supervisor &&
+  pip3 freeze > $SCRIPT_DIR/requirements.txt &&
   # make wheel
-  # git clone https://github.com/Supervisor/supervisor.git
-  # cd supervisor
-  # python setup.py bdist_wheel
-  #https://stackoverflow.com/questions/19796883/supervisord-for-python-3
+  # refs:  https://stackoverflow.com/questions/19796883/supervisord-for-python-3
+  TARGET=$BUILD_DIR/supervisor &&
+  if [[ -d $TARGET ]]; then rm -rf $TARGET; fi &&
+  git clone https://github.com/Supervisor/supervisor.git $TARGET &&
+  cd $TARGET &&
+  python setup.py bdist_wheel &&
+  cp -p $TARGET/dist/supervisor-*.whl $WHEELS_DIR
 fi
+RESULT=$?
+
+popd
+
+log $TARGET
+exit $RESULT
