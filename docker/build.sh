09a2a0b56b059fb6fae3d014e11786b9e23dfee2
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-01-14 11:40:05 +0900

    fix: Return status when build failed in docker

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,9 @@ case "$1" in
   centos6|ubuntu1404)
     pushd ${SCRIPT_DIR}
     docker-compose run --rm $1 /opt/${TARGET}/workspace/scripts/build-all.sh
+    RESULT=$?
     popd
+    exit ${RESULT}
     ;;
   *)
     echo "Usage: $0 [centos6|ubuntu1404]"
