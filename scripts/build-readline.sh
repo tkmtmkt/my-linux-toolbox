9bcbecdd7850ac0a93670e7e43281e2999b8ef27
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-02-24 20:19:38 +0900

    modify: readline (8.0 <- 7.0)

--- scripts/build-readline.sh
+++ scripts/build-readline.sh
@@ -3,20 +3,13 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=7.0
+VERSION=8.0
 TARGET=readline-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
 DOWNLOAD_URL=http://ftp.gnu.org/gnu/readline/${TARGET}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL -o ${ARCHIVE} ${DOWNLOAD_URL}
-
-for n in 001 002 003 004 005
-do
-  PATCH="readline70-$n"
-  [[ ! -s "${ARCHIVES_DIR}/${PATCH}" ]] &&
-    curl -ksSL  http://ftp.gnu.org/gnu/readline/readline-7.0-patches/${PATCH} -o "${ARCHIVES_DIR}/${PATCH}"
-done
 
 pushd ${BUILD_DIR}
 
@@ -26,10 +19,6 @@ tar xf ${ARCHIVE}
 cd ${TARGET}
 
 # build
-for n in 001 002 003 004 005
-do
-  patch -p0 < ${ARCHIVES_DIR}/readline70-$n
-done &&
 ./configure --prefix=${PREFIX} \
             --enable-multibyte &&
 make && make install
