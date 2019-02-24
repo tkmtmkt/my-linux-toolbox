f41e48b6ebcd5c8685ae79ae5d9b4ccba17aeff9
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-02-24 11:02:20 +0900

    modify: update nkf (2.1.5 <- 2.1.4)

--- scripts/build-nkf.sh
+++ scripts/build-nkf.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=2.1.4
+VERSION=2.1.5
 TARGET=nkf-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
-DOWNLOAD_URL=https://ja.osdn.net/projects/nkf/downloads/64158/${TARGET}.tar.gz
+DOWNLOAD_URL=https://ja.osdn.net/projects/nkf/downloads/70406/${TARGET}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
