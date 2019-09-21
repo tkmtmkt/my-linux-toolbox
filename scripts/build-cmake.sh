4ecd495218429fbfa6f545bbe35f782fe4c565a9
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-09-21 23:19:07 +0900

    modify: update cmake (3.15.3 <- 3.14.5)

--- scripts/build-cmake.sh
+++ scripts/build-cmake.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=3.14.5
-TARGET=cmake-${VERSION}
+VERSION=3.15.3
+TARGET=cmake-${VERSION}-Linux-x86_64
 
 # download
-ARCHIVE=${ARCHIVES_DIR}/${TARGET}-linux-x64-${VERSION}.tar.gz
-DOWNLOAD_URL=https://github.com/Kitware/CMake/releases/download/v${VERSION}/${TARGET}-Linux-x86_64.tar.gz
+ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
+DOWNLOAD_URL=https://github.com/Kitware/CMake/releases/download/v${VERSION}/${TARGET}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
@@ -17,7 +17,8 @@ pushd ${BUILD_DIR}
 [[ -e ${TARGET} ]] && rm -rf ${TARGET}
 tar xf ${ARCHIVE}
 
-cp -rp ${TARGET}-Linux-x86_64/* ${PREFIX}/
+# build
+cp -rp ${TARGET}/* ${PREFIX}/
 RESULT=$?
 
 popd
