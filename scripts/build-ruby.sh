ef3f7c1be145da8ea0ac9b1a7313ecaad0b96ac2
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-11-04 11:52:52 +0900

    modify: upate ruby (2.6.5 <- 2.6.4)

--- scripts/build-ruby.sh
+++ scripts/build-ruby.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
 VERSION=2.6
-TARGET=ruby-${VERSION}.4
+TARGET=ruby-${VERSION}.5
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.xz
