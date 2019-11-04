d5344f838c898307d0bf90539b7317a65dbd9412
Takamatsu Makoto <tkmtmkt@gmail.com>
2019-11-04 11:54:30 +0900

    modify: update cmake (3.15.5 <- 3.15.3)

--- scripts/build-neomutt.sh
+++ scripts/build-neomutt.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=20180716
-TARGET=neomutt-neomutt-${VERSION}
+VERSION=20191102
+TARGET=neomutt-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/neomutt-${VERSION}.tar.gz
-DOWNLOAD_URL=https://github.com/neomutt/neomutt/archive/neomutt-${VERSION}.tar.gz
+DOWNLOAD_URL=https://github.com/neomutt/neomutt/archive/${VERSION}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
