adb501193b4791b63ac5d8da7747aed52d9322a4
Takamatsu Makoto <tkmtmkt@gmail.com>
2020-04-14 08:50:37 +0900

    modify: update fio (3.19 <- 3.17)

--- scripts/build-fio.sh
+++ scripts/build-fio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# https://github.com/git/git/releases
+# https://github.com/axboe/fio/releases
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=3.17
+VERSION=3.19
 TARGET=fio-${VERSION}
 
 # download
