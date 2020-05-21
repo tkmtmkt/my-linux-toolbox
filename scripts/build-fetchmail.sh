035e8d5977a0f89185d71e49f4b71fb0212f65fa
Takamatsu Makoto <tkmtmkt@gmail.com>
2020-05-21 12:49:44 +0900

    modify: update fetchmail (6.4.5 <- 6.4.3)

--- scripts/build-fetchmail.sh
+++ scripts/build-fetchmail.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
 VERSION=6.4
-TARGET=fetchmail-${VERSION}.3
+TARGET=fetchmail-${VERSION}.5
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.xz
-DOWNLOAD_URL=https://sourceforge.net/projects/fetchmail/files/branch_${VERSION}/fetchmail-${VERSION}.3.tar.xz/download
+DOWNLOAD_URL=https://sourceforge.net/projects/fetchmail/files/branch_${VERSION}/${TARGET}.tar.xz/download
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
