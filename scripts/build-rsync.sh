ddad384adb0ef62bf0142721517e9fb91d78f20f
Takamatsu Makoto <tkmtmkt@gmail.com>
2020-09-28 01:56:42 +0900

    modify: update rsync (3.2.3 <- 3.2.1)

--- scripts/build-rsync.sh
+++ scripts/build-rsync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# https://download.samba.org/pub/rsync/src/
+# https://download.samba.org/pub/rsync/src/?C=M;O=D
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=3.2.1
+VERSION=3.2.3
 TARGET=rsync-${VERSION}
 
 # download
