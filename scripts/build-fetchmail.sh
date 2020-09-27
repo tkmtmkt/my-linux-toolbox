2c6c5d03834e1eb46446921a1371a83af1a41c96
Takamatsu Makoto <tkmtmkt@gmail.com>
2020-09-28 02:23:54 +0900

    modify: update fetchmail (6.4.12 <- 6.4.8)

--- scripts/build-fetchmail.sh
+++ scripts/build-fetchmail.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
 VERSION=6.4
-TARGET=fetchmail-${VERSION}.8
+TARGET=fetchmail-${VERSION}.12
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.xz
