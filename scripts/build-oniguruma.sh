902519e1473da1aca59ced07e14bbb5f931a8b72
Takamatsu Makoto <tkmtmkt@gmail.com>
2021-07-13 01:03:08 +0900

    modify: update oniguruma (6.9.7.1 <- 6.9.6)

--- scripts/build-oniguruma.sh
+++ scripts/build-oniguruma.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=6.9.6
+VERSION=6.9.7
 TARGET=onig-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
-DOWNLOAD_URL=https://github.com/kkos/oniguruma/releases/download/v${VERSION}/${TARGET}.tar.gz
+DOWNLOAD_URL=https://github.com/kkos/oniguruma/releases/download/v${VERSION}.1/${TARGET}.1.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
