ce4818337d63f48e239e75c46493cf915084b992
Takamatsu Makoto <tkmtmkt@gmail.com>
2021-07-13 00:02:09 +0900

    modify: Add libicu-dev to thedocker image

--- docker/centos/assets/setup.sh
+++ docker/centos/assets/setup.sh
@@ -6,6 +6,7 @@ yum -q -y install wget &&
 yum -q -y install make gcc gcc-c++ patch &&
 yum -q -y install flex &&
 yum -q -y install cmake libtool libuuid-devel &&
+yum -q -y install libicu-devel &&
 yum -q -y install centos-release-scl &&
 yum -q -y install devtoolset-8 &&
 
--- docker/ubuntu/assets/setup.sh
+++ docker/ubuntu/assets/setup.sh
@@ -8,6 +8,7 @@ apt -q -y install make g++ patch &&
 apt -q -y install flex &&
 apt -q -y install groff-base &&
 apt -q -y install cmake uuid-dev &&
+apt -q -y install libicu-dev &&
 
 # Remove installation files
 rm -r /assets/
