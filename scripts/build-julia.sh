c3f25514cdde4757f412f182a8fccb1c7e0ae4ec
Takamatsu Makoto <tkmtmkt@gmail.com>
2022-01-08 22:32:10 +0900

    modify: update julia (1.6.5 <- 1.6.2)

--- scripts/build-julia.sh
+++ scripts/build-julia.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
 VERSION=1.6
-TARGET=julia-${VERSION}.2
+TARGET=julia-${VERSION}.5
 
 # download
 case $(uname -m) in
