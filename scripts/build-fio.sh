8d6b9a4086e6e149045655322ebac1432f9f38e5
Takamatsu Makoto <tkmtmkt@gmail.com>
2022-01-08 22:38:13 +0900

    modify: update fio (3.29 <- 3.28)

--- scripts/build-fio.sh
+++ scripts/build-fio.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=3.28
+VERSION=3.29
 TARGET=fio-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
-DOWNLOAD_URL=https://github.com/axboe/fio/archive/${TARGET}.tar.gz
+DOWNLOAD_URL=https://github.com/axboe/fio/archive/refs/tags/${TARGET}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
