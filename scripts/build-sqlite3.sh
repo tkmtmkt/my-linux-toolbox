95fda3d105083059f7a0d96e709c9a4df957cb8b
Takamatsu Makoto <tkmtmkt@gmail.com>
2022-01-08 22:31:18 +0900

    modify: update sqlite3 (3370200 <- 3360000)

--- scripts/build-sqlite3.sh
+++ scripts/build-sqlite3.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=3360000
+VERSION=3370200
 TARGET=sqlite-autoconf-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
-DOWNLOAD_URL=https://www.sqlite.org/2021/${TARGET}.tar.gz
+DOWNLOAD_URL=https://www.sqlite.org/2022/${TARGET}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL -o ${ARCHIVE} ${DOWNLOAD_URL}
 
 pushd ${BUILD_DIR}
