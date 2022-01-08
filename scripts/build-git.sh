921c2e2b8f88916ca8219c5d8bba5d67d454500a
Takamatsu Makoto <tkmtmkt@gmail.com>
2022-01-08 22:34:48 +0900

    modify: update git (2.34.1 <- 2.33.0)

--- scripts/build-git.sh
+++ scripts/build-git.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=2.33.0
+VERSION=2.34.1
 TARGET=git-${VERSION}
 
 # download
 ARCHIVE=${ARCHIVES_DIR}/${TARGET}.tar.gz
-DOWNLOAD_URL=https://github.com/git/git/archive/v${VERSION}.tar.gz
+DOWNLOAD_URL=https://github.com/git/git/archive/refs/tags/v${VERSION}.tar.gz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL ${DOWNLOAD_URL} -o ${ARCHIVE}
 
 pushd ${BUILD_DIR}
