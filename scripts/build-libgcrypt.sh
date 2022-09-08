32a072516b9b508f04a7d90a57b67e5bfcae8d44
Takamatsu Makoto <tkmtmkt@gmail.com>
2022-09-08 21:29:30 +0900

    modify: update gnupg(2.2.36 <- 2.2.31), libgpg-error(1.45 <- 1.42), libgcrypt(1.8.9 <- 1.8.8), ntbtls(0.3.1 <- 0.2.0)

--- scripts/build-libgcrypt.sh
+++ scripts/build-libgcrypt.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=1.8.8
+VERSION=1.8.9
 TARGET=libgcrypt-${VERSION}
 
 # download
--- scripts/build-ntbtls.sh
+++ scripts/build-ntbtls.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=0.2.0
+VERSION=0.3.1
 TARGET=ntbtls-${VERSION}
 
 # download
