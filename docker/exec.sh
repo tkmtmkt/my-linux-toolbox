caab0dd0d72eb990b692df6be89a027d8ad484e0
Takamatsu Makoto <tkmtmkt@gmail.com>
2023-05-06 03:09:26 +0900

    modify: Change container tool from docker to podman

--- docker/exec.sh
+++ docker/exec.sh
@@ -2,18 +2,30 @@
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 
 export PREFIX=/opt/toolbox
-export WORKSPACE=workspace
+export WORKSPACE=${PREFIX}/workspace
+export HOST=$1
 
-case "$1" in
-  centos|ubuntu)
-    pushd ${SCRIPT_DIR} > /dev/null
-    docker-compose run --rm $1 /bin/bash
-    RESULT=$?
-    popd > /dev/null
-    exit ${RESULT}
+case "${HOST}" in
+  centos)
+    IMAGE=centos_toolbox:7.7.1908
+    ;;
+  ubuntu)
+    IMAGE=ubuntu_toolbox:18.04
     ;;
   *)
     echo "Usage: $0 [centos|ubuntu]"
     exit 1
     ;;
 esac
+
+pushd ${SCRIPT_DIR} > /dev/null
+#docker-compose run --rm $1 /bin/bash
+[[ ! -d ${HOST}${PREFIX} ]] && mkdir -p ${HOST}${PREFIX}
+podman run -it --rm -h ${HOST} \
+  -v "./${HOST}${PREFIX}/:${PREFIX}/:rw" \
+  -v "../scripts/:${WORKSPACE}/scripts/:rw" \
+  -v "../archives/:${WORKSPACE}/archives/:rw" \
+  ${IMAGE} /bin/bash
+RESULT=$?
+popd > /dev/null
+exit ${RESULT}
