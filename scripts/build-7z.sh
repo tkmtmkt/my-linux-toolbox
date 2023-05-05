cfc4645417bda358c87ffae20b60af18b5d7af90
Takamatsu Makoto <tkmtmkt@gmail.com>
2023-05-05 21:26:13 +0900

    modify: udpate 7z (2201 <- 16.02)

--- scripts/build-7z.sh
+++ scripts/build-7z.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-# https://sourceforge.net/projects/p7zip/files/p7zip/
+# https://sevenzip.osdn.jp/download.html
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 source ${SCRIPT_DIR}/build-common.sh
 
-VERSION=16.02
-TARGET=p7zip_${VERSION}
+VERSION=2201
+TARGET=7z${VERSION}
 
 # download
-ARCHIVE=${ARCHIVES_DIR}/${TARGET}_src_all.tar.bz2
-DOWNLOAD_URL=https://sourceforge.net/projects/p7zip/files/p7zip/${VERSION}/${TARGET}_src_all.tar.bz2
+ARCHIVE=${ARCHIVES_DIR}/${TARGET}-linux-x64.tar.xz
+DOWNLOAD_URL=https://www.7-zip.org/a/${TARGET}-linux-x64.tar.xz
 [[ ! -s ${ARCHIVE} ]] && curl -ksSL -o ${ARCHIVE} ${DOWNLOAD_URL}
 
 pushd ${BUILD_DIR}
 
 # expand
 [[ -d ${TARGET} ]] && rm -rf ${TARGET}
-tar xf ${ARCHIVE}
-cd ${TARGET}
+mkdir ${TARGET}
+tar xf ${ARCHIVE} -C ${TARGET}
 
-# build
-sed -i "s|/usr/local|${PREFIX}|g" makefile.common &&
-make all3 && make install
+# build (download only)
+cp -rp ${TARGET}/7zzs ${PREFIX}/bin/7z
 RESULT=$?
 
 popd
