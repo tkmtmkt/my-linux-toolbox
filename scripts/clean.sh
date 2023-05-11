77fe30e97559af00d6d57f5512b4292846a869e8
Takamatsu Makoto <tkmtmkt@gmail.com>
2023-05-11 18:36:58 +0900

    modify: Change to delete everything except workspace

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,19 +6,19 @@ set -o pipefail
 
 SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0});pwd)
 WORKSPACE_DIR=$(dirname ${SCRIPT_DIR})
+WORKSPACE_NAME=$(basename ${WORKSPACE_DIR})
 TARGET_DIR=$(dirname ${WORKSPACE_DIR})
 
 rm -rf ${WORKSPACE_DIR}/log/* \
        ${WORKSPACE_DIR}/build/
-rm -rf ${TARGET_DIR}/BUILDLOG \
-       ${TARGET_DIR}/bin \
-       ${TARGET_DIR}/buildtool \
-       ${TARGET_DIR}/etc/ \
-       ${TARGET_DIR}/include/ \
-       ${TARGET_DIR}/lib/ \
-       ${TARGET_DIR}/libexec/ \
-       ${TARGET_DIR}/man/ \
-       ${TARGET_DIR}/sbin/ \
-       ${TARGET_DIR}/share/ \
-       ${TARGET_DIR}/ssl/ \
-       ${TARGET_DIR}/var/
+
+for DIR in $(ls ${TARGET_DIR})
+do
+  case ${DIR} in
+    "${WORKSPACE_NAME}")
+      ;;
+    *)
+      rm -rf ${TARGET_DIR}/${DIR}
+      ;;
+  esac
+done
